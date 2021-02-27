7ff0ec34cd1403d8306b610b87aa0fac0e2da2fc
Quentin Castel <quentincastel86@gmail.com>
2021-02-27 17:14:09 +0000

    debug

--- mvn-action.sh
+++ mvn-action.sh
@@ -6,7 +6,7 @@ pwd
 
 if [ -d "${M2_HOME_FOLDER}" ]; then
      echo "INFO - M2 folder '${M2_HOME_FOLDER}' not empty. We therefore will beneficy from the CI cache";
-     ls -l ${M2_HOME_FOLDER}/.m2;
+     ls -l ${M2_HOME_FOLDER};
 else
      echo "WARN - No M2 folder '${M2_HOME_FOLDER}' found. We therefore won't beneficy from the CI cache";
 fi
